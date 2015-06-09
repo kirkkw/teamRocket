941b08ec7f8afbd65421fd25727fff7ace4af9fc
Michael Lee <mil039@ucsd.edu>
2015-06-09 15:46:35 -0700

    making it live

--- public/js/controller.js
+++ public/js/controller.js
@@ -53,16 +53,16 @@ $(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</
   if(questionLock==false){questionLock=true;  
   //correct answer
   if(this.id==1){
-   score = 5 + score;
+   score = 4 + score;
    }
   if(this.id==2){
-   score = 10 + score;
+   score = 9 + score;
    }
   if(this.id==3){
-   score = 15 + score;
+   score = 14 + score;
    } 
   if(this.id==4){
-   score = 2 + score;
+   score = 1 + score;
    } 
 
   setTimeout(function(){changeQuestion()},200);
@@ -92,8 +92,23 @@ $(stage).append('<div class="questionText">'+questionBank[questionNumber][0]+'</
   
   function displayFinalSlide(){
     
-    $(stage).append('<div class="questionText">You have finished the quiz!<br><br>Total questions: '+numberOfQuestions+'<br>You are this likely to smoke: '+score+'</div>');
-    
+    if(score > 80)
+    {
+      $(stage).append('<div class="questionText">YIKES, you better becareful! Chances are strong that you probably will become a smoker!<br><br>You have a <b>'+score+' %</b> chance of becoming a smoker<br>Say No to Smoking! Unless you want to be like Bobby Hill<br><img src="img/bobbyHill.gif"></div>');
+    }
+    if(50 < score && score < 80)
+    {
+      $(stage).append('<div class="questionText"> Its not looking so good, watch out there!<br><br>You have a <b>'+score+' %</b> chance of becoming a smoker<br>Do you want to turn out like Miley Cyrus?<br><img src="img/mileyCyrus.gif"></div>');
+    }
+    if(20 < score && score <50)
+    {
+      $(stage).append('<div class="questionText"> You might be a smoker, so keep saying No to cigarettes!<br><br>You have a <b>'+score+' %</b> chance of becoming a smoker<br>See no one likes to smoke, just look at Tia Mowry coughing up her lung<br><img src="img/tiaMowry.gif"></div>');
+    }
+    if(score < 20)
+    {
+      $(stage).append('<div class="questionText">Youre not around smokers that often, so you dont have too much to worry about, but keep saying no!<br><br>You have a <b>'+score+' %</b> chance of becoming a smoker<br>Be like Conan and say NO to Smoking!<br><img src="img/conan.gif"></div>');
+    }
+
   }//display final slide
   
   
